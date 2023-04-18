d983f8c61ed2498f80f09fedb3717b76136d79fd
Judy Ye <judyy.ye@gmail.com>
2023-04-18 12:47:20 -0700

    add reassigned string variables

--- variables.js
+++ variables.js
@@ -1,8 +1,13 @@
 var username = "judyy.ye"
+username = "judy"
 var email = "judyy.ye@gmail.com"
+email = "boop@boop.com"
 
 var age = 32
 var bankAcctBal = -10000
 
 var isConfused = true
 var isSleepy = true
+
+console.log(username)
+console.log(email)
